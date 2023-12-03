d4cfdb2990acb69e0069bfc465d16625a664a147
Rait Talisoo <rait.talisoo@gmail.com>
2023-12-03 20:21:08 +0200

    Derpy front goes brr

--- server/wwwroot/js/index.js
+++ server/wwwroot/js/index.js
@@ -1,28 +1,32 @@
 function uploadFile() {
-    const fileInput = document.getElementById('fileInput');
-  
-    if (fileInput.files.length === 0) {
-      alert('Faili tahaks kangesti');
-      return;
-    }
+  const fileInput = document.getElementById("fileInput");
 
-    const formData = new FormData();
-    formData.append('File', fileInput.files[0]);
-
-    fetch('/predict', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => response.json())
-    .then(data => {
-      const resultContainer = document.getElementById('resultContainer');
-      resultContainer.innerHTML = '<h2>Ennustus:</h2>'
-         + "Tõenäosus, et on ehitud:" + (data.decorated * 100).toFixed(2) + "%<br>"
-         + "Tõenäosus, et on ehtimata:" + (data.undecorated * 100).toFixed(2) + "%" ;
-
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      alert('Ikaldus. Ei tea.');
-    });
+  if (fileInput.files.length === 0) {
+    alert("Faili tahaks kangesti");
+    return;
   }
+
+  const formData = new FormData();
+  formData.append("File", fileInput.files[0]);
+
+  fetch("/predict", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const resultContainer = document.getElementById("resultContainer");
+      resultContainer.innerHTML =
+        '<div class="col-12"><h2>Ennustus:</h2>' +
+        "Tõenäosus, et on ehitud: " +
+        (data.decorated * 100).toFixed(2) +
+        '% </div class="col-12">' +
+        "<div>Tõenäosus, et on ehtimata: " +
+        (data.undecorated * 100).toFixed(2) +
+        "% </div>";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("Ikaldus. Ei tea.");
+    });
+}
